9cfa01ca45f04f7f86fd4e05f3f52f4a634ec5b4
Meriem Khartaoui <meriemkhartaoui@outlook.it>
2023-12-16 13:27:02 +0100

    Initial commit

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -12,17 +12,16 @@ import java.util.List;
 @SpringBootApplication
 public class 	CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
 	@Bean
 	CommandLineRunner start(CustomerRepository customerRepository) {
 		return args -> {
 			customerRepository.saveAll(List.of(
-					Customer.builder().name("youness").email("uness@gmail.com").build(),
-					Customer.builder().name("ashraf").email("asshraf@gmail.com").build(),
-					Customer.builder().name("ibrahim").email("ibrahim@gmail.com").build()
+					Customer.builder().name("meriem").email("meriem@gmail.com").build(),
+					Customer.builder().name("yassir").email("yassir@gmail.com").build(),
+					Customer.builder().name("salma").email("salma@gmail.com").build()
 			));
 			customerRepository.findAll().forEach(System.out::println);
 		};
